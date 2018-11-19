6c1bb4d64b843a4c1fbcd48fc7eb6ac5da09db4e
Alexander Wunschik <alex@wunschik.net>
2018-11-19 22:03:30 +0100

    minor improvements

--- src/util.js
+++ src/util.js
@@ -2,6 +2,7 @@ import Currency from 'currency-formatter'
 
 const pwrap = fn => (req, res, next) => fn(req, res).catch(next)
 
-const fiatFormatter = currency => amount => Currency.format(amount, { code: currency.toUpperCase() })
+//const fiatFormatter = currency => amount => Currency.format(amount, { code: currency.toUpperCase() })
+const fiatFormatter = currency => amount => new Intl.NumberFormat('de-DE', { style: 'currency', currency: currency.toUpperCase() }).format(amount);
 
 module.exports = { pwrap, fiatFormatter }
